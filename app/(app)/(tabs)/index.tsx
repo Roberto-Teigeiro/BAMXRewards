469fdb7d208242f583119a01dedb7b7c25ebd7ba
Luis Omar Olmedo Ortiz <133702840+Luisssso2003@users.noreply.github.com>
2024-10-09 20:23:29 +0000

    Conexión de tiendas y productos desde la base de datos.

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,37 +1,71 @@
-import React from 'react';
-import { FlatList, StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StyleSheet, Text, SafeAreaView, Image } from 'react-native';
+import { createClient } from '@supabase/supabase-js';
 import CustomCard from '@/components/ui/CustomCard';
 
-const partners = [
-  { id: '1', logo: require('@/assets/images/oxxo.png'), name: 'Oxxo' },
-  { id: '2', logo: require('@/assets/images/walmart.png'), name: 'Walmart' },
-  { id: '3', logo: require('@/assets/images/cinepolis.png'), name: 'Cinepolis' },
-  { id: '4', logo: require('@/assets/images/soriana.png'), name: 'Soriana' },
-  { id: '5', logo: require('@/assets/images/costco.png'), name: 'Costco' },
-  { id: '6', logo: require('@/assets/images/merkabastos.png'), name: 'Merkabastos' },
-  { id: '7', logo: require('@/assets/images/chedraui.png'), name: 'Chedraui' },
-  { id: '8', logo: require('@/assets/images/okpasteleria.png'), name: 'OK Pastelería' },
-  { id: '9', logo: require('@/assets/images/lagorda.png'), name: 'La Gorda' },
-  { id: '10', logo: require('@/assets/images/elpanque.png'), name: 'El Panqué' },
-  { id: '11', logo: require('@/assets/images/panbueno.png'), name: 'Pan Bueno' },
-  { id: '12', logo: require('@/assets/images/manhattan.png'), name: 'Paletas Manhattan' },
-  { id: '13', logo: require('@/assets/images/cinemex.png'), name: 'Cinemex' },
-  { id: '14', logo: require('@/assets/images/ohlala.png'), name: 'Ohlala!' },
-  // Add other partners here
-];
+// Asegúrate de reemplazar estos valores con tus propias credenciales de Supabase
+const supabaseUrl = 'https://lihzaprklohfdxnnudzh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpaHphcHJrbG9oZmR4bm51ZHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2NzQzMzcsImV4cCI6MjA0MjI1MDMzN30.b8i2VyqTGQgxZ2tndE11H0xTAjEKVtHLYkzAs__WOSw';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+interface Retailer {
+  id: number;
+  name: string;
+  logo_path: string;
+}
+
+// Objeto de mapeo para las imágenes
+const images = {
+  'oxxo.png': require('@/assets/images/oxxo.png'),
+  'walmart.png': require('@/assets/images/walmart.png'),
+  'cinepolis.png': require('@/assets/images/cinepolis.png'),
+  'soriana.png': require('@/assets/images/soriana.png'),
+  'costco.png': require('@/assets/images/costco.png'),
+  'merkabastos.png': require('@/assets/images/merkabastos.png'),
+  'chedraui.png': require('@/assets/images/chedraui.png'),
+  'okpasteleria.png': require('@/assets/images/okpasteleria.png'),
+  'lagorda.png': require('@/assets/images/lagorda.png'),
+  'elpanque.png': require('@/assets/images/elpanque.png'),
+  'panbueno.png': require('@/assets/images/panbueno.png'),
+  'manhattan.png': require('@/assets/images/manhattan.png'),
+  'cinemex.png': require('@/assets/images/cinemex.png'),
+  'ohlala.png': require('@/assets/images/ohlala.png'),
+};
 
 const App = () => {
+  const [retailers, setRetailers] = useState<Retailer[]>([]);
+
+  useEffect(() => {
+    fetchRetailers();
+  }, []);
+
+  async function fetchRetailers() {
+    const { data, error } = await supabase
+      .from('retailers')
+      .select('*');
+    if (error) {
+      console.error('Error fetching retailers:', error);
+    } else {
+      console.log('Retailers:', data);
+      setRetailers(data as Retailer[]);
+    }
+  }
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <FlatList
-        ListHeaderComponent={<Text style={styles.title}>Nuestros Aliados</Text>} // Coloca el título como parte del contenido desplazable
-        data={partners}
-        renderItem={({ item }) => (
-          <CustomCard logo={item.logo} name={item.name} />
+        ListHeaderComponent={<Text style={styles.title}>Nuestros Aliados</Text>}
+        data={retailers}
+        renderItem={({ item }: { item: Retailer }) => (
+          <CustomCard 
+            logo={images[item.logo_path as keyof typeof images]}
+            name={item.name} 
+          />
         )}
-        numColumns={2} // Cambia esto según sea necesario
-        keyExtractor={item => item.id}
-        contentContainerStyle={styles.contentContainer} // Asegura que el contenido tenga un padding adecuado
+        numColumns={2}
+        keyExtractor={(item: Retailer) => item.id.toString()}
+        contentContainerStyle={styles.contentContainer}
       />
     </SafeAreaView>
   );
--- app/(app)/(tabs)/store/[param].tsx
+++ app/(app)/(tabs)/store/[param].tsx
@@ -7,10 +7,21 @@ import { useSession } from '@/context';
 import StoreCard from "@/components/ui/storeCard"; 
 import QRCodeComponent from '@/components/ui/QRCode'; 
 
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  logo_path: string;
+}
+
 export default function MyComponent() {
   const { param } = useLocalSearchParams();
   const [session, setSession] = useState<Session | null>(null);
   const { signOut } = useSession();
+  const [loading, setLoading] = useState(true);
+  const [qrVisible, setQrVisible] = useState(false);
+  const [selectedProductData, setSelectedProductData] = useState('');
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -22,120 +33,102 @@ export default function MyComponent() {
     });
   }, []);
 
-  const [loading, setLoading] = useState(true);
-  const [username, setUsername] = useState('');
-  const [website, setWebsite] = useState('');
-  const [avatarUrl, setAvatarUrl] = useState('');
-  const [fullName, setfullName] = useState('');
-  const [qrVisible, setQrVisible] = useState(false);
-  const [selectedProductData, setSelectedProductData] = useState('');
-
-  const productos: { [key: string]: { [key: string]: { description: string; logo: any } } } = {
-    "Oxxo": {
-      "Doritos": { description: "Description of Product 1", logo: require('@/assets/images/doritos.png') },
-      "Pringles": { description: "Description of Product 2", logo: require('@/assets/images/pringles.png') },
-      "Papas Sabritas": { description: "Description of Product 3", logo: require('@/assets/images/papas.png') },
-      "Chocolate Abuelita": { description: "Description of Product 4", logo: require('@/assets/images/chocolate.png') },
-      "Gatorlyte": { description: "Description of Product 5", logo: require('@/assets/images/gatorlyte.png') },
-    },
-    "Walmart": {
-      "Nombre1": { description: "Description of Product 1", logo: require('@/assets/images/favicon-scaled.png') },
-      "Nombre2": { description: "Description of Product 2", logo: require('@/assets/images/favicon-scaled.png') },
-      "Nombre3": { description: "Description of Product 3", logo: require('@/assets/images/favicon-scaled.png') },
-    },
-    "Cinepolis": {
-      "Nombre1": { description: "Description of Product 1", logo: require('@/assets/images/favicon-scaled.png') },
-      "Nombre2": { description: "Description of Product 2", logo: require('@/assets/images/favicon-scaled.png') },
-    },
-    "Soriana": {
-      "Nombre1": { description: "Description of Product 1", logo: require('@/assets/images/favicon-scaled.png') },
-      "Nombre2": { description: "Description of Product 2", logo: require('@/assets/images/favicon-scaled.png') },
-      "Nombre3": { description: "Description of Product 3", logo: require('@/assets/images/favicon-scaled.png') },
-    },
-    "Costco": {
-      "Nombre1": { description: "Description of Product 1", logo: require('@/assets/images/favicon-scaled.png') },
-      "Nombre2": { description: "Description of Product 2", logo: require('@/assets/images/favicon-scaled.png') },
-    },
-    "Merkabastos": {
-      "Nombre1": { description: "Description of Product 1", logo: require('@/assets/images/favicon-scaled.png') },
-      "Nombre2": { description: "Description of Product 2", logo: require('@/assets/images/favicon-scaled.png') },
-    }
-  };
-
   useEffect(() => {
-    if (session) getProfile();
-  }, [session]);
+    if (param) {
+      fetchProductsByRetailerNameOrId(param as string);
+    } else {
+      console.error('No retailer parameter provided');
+    }
+  }, [param]);
 
-  async function getProfile() {
+  async function fetchProductsByRetailerNameOrId(retailerParam: string) {
     try {
       setLoading(true);
-      if (!session?.user) throw new Error('No user on the session!');
+      let retailerId: number | null = null;
 
-      const { data, error, status } = await supabase
-        .from('profiles')
-        .select(`username, website, avatar_url, full_name`)
-        .eq('id', session?.user.id)
-        .single();
-      if (error && status !== 406) {
-        throw error;
+      // Primero, intentamos convertir el parámetro a un número
+      const parsedId = parseInt(retailerParam, 10);
+      if (!isNaN(parsedId)) {
+        retailerId = parsedId;
+      } else {
+        // Si no es un número, asumimos que es un nombre y buscamos el ID correspondiente
+        const { data: retailers, error: retailerError } = await supabase
+          .from('retailers')
+          .select('id')
+          .eq('name', retailerParam)
+          .single();
+
+        if (retailerError) throw retailerError;
+        if (retailers) retailerId = retailers.id;
       }
 
-      if (data) {
-        setUsername(data.username);
-        setWebsite(data.website);
-        setAvatarUrl(data.avatar_url);
-        setfullName(data.full_name);
+      if (retailerId === null) {
+        throw new Error(`No retailer found for: ${retailerParam}`);
       }
+
+      const { data: products, error } = await supabase
+        .from('products')
+        .select('*')
+        .eq('retailer_id', retailerId);
+
+      if (error) throw error;
+      console.log(`Products for retailer ${retailerParam}:`, products);
+      setProducts(products || []);
     } catch (error) {
-      if (error instanceof Error) {
-        Alert.alert(error.message);
-      }
+      console.error(`Error fetching products for retailer ${retailerParam}:`, error);
+      Alert.alert('Error', 'Unable to fetch products. Please try again later.');
     } finally {
       setLoading(false);
     }
   }
-
-  // Check if param is a string or an array of strings
-  const paramValue = Array.isArray(param) ? param[0] : param;
-
-  // Get products for the corresponding store
-  const storeProducts = paramValue && productos[paramValue] 
-    ? Object.entries(productos[paramValue]) 
-    : [];
 
   const handleProductPress = (productName: string) => {
     setSelectedProductData(productName);
     setQrVisible(true);
   };
 
+  // Objeto de mapeo para las imágenes de los productos
+  const productImages: { [key: string]: any } = {
+    'doritos.png': require('@/assets/images/doritos.png'),
+    'pringles.png': require('@/assets/images/pringles.png'),
+    'papas.png': require('@/assets/images/papas.png'),
+    'chocolate.png': require('@/assets/images/chocolate.png'),
+    'gatorlyte.png': require('@/assets/images/gatorlyte.png'),
+    'favicon-scaled.png': require('@/assets/images/favicon-scaled.png'),
+  };
+
+  // Suponiendo que quieres obtener el nombre de la tienda de los productos
+  const storeName = param as string; // Puedes ajustar esto según cómo obtengas el nombre de la tienda
+
   return (
     <View>
-      {paramValue && storeProducts.length > 0 ? (
+      {loading ? (
+        <Text>Loading products...</Text>
+      ) : products.length > 0 ? (
         <FlatList
-          data={storeProducts}
+          data={products}
           numColumns={2} 
-          keyExtractor={([key]) => key} 
+          keyExtractor={(item) => item.id.toString()} 
           renderItem={({ item }) => (
             <StoreCard 
-              logo={item[1].logo} 
-              name={item[0]} 
-              description={item[1].description} 
-              onPress={() => handleProductPress(item[0])} 
+              logo={productImages[item.logo_path] || require('@/assets/images/favicon-scaled.png')}
+              name={item.name} 
+              description={item.description} 
+              onPress={() => handleProductPress(item.name)} 
             />
           )}
         />
       ) : (
-        <Text>No valid param or no products available</Text>
+        <Text>No products available for this store</Text>
       )}
 
-      {/* QR Code modal for selected product */}
       <QRCodeComponent 
         visible={qrVisible} 
         onClose={() => setQrVisible(false)} 
         productData={selectedProductData} 
+        store={storeName}
       />
 
-      <Text>{session?.user?.email}</Text>
       <Button title="Go to Store" onPress={() => console.log("Navigating to Store...")} />
     </View>
   );
